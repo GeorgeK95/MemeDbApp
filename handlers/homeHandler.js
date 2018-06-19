a1557bf53c5a2d41163fe70bd9894360a57d195a
GeorgeK95 <george_it@abv.bg>
2018-06-19 21:33:23 +0300

    added gf wish

--- handlers/homeHandler.js
+++ handlers/homeHandler.js
@@ -1,6 +1,8 @@
 module.exports.index = (req, res) => {
     if (req.originalUrl === '/' && req.method === 'GET') {
         res.render('home/home')
+    } else if (req.originalUrl === '/goodluck' && req.method === 'GET') {
+        res.render('ztmm/goodluck')
     } else {
         return true
     }
